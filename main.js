b1b217522cf852105fc5bce06a759ee37249b462
courtneysembly <courtneysembly@gmail.com>
2023-11-27 18:38:03 -0500

    search results WIP

--- main.js
+++ main.js
@@ -1,63 +1,47 @@
-const itunesUrl = "https://proxy-itunes-api.glitch.me/";
-let inputForm = document.getElementById('Form');
-let inputField = document.getElementById('inputField');
-let results = document.getElementById('results');
-let button = document.getElementById('searchButton');
-let container = document.getElementById("container");
-let playPreview = document.getElementById("songUrl");
-let songTitle = document.getElementById("songName")
-const songUrl = "https://geo.itunes.apple.com/us/album/";
+const url = "https://itunes.apple.com/search?limit=15&media=music&term=";
+let userInput = document.getElementById("userInput");
+let save = document.getElementById("submit");
+let inputField = document.getElementById("inputField");
+const artistResult = document.getElementById("result");
+const audioPlayers = document.getElementById("topAudio")
+
+const audioPlay = document.createElement('audio');
+
+audioPlay.controls = true;
+audioPlayers.appendChild(audioPlay)
+userInput.addEventListener("click", (event) => {
+  event.preventDefault();
+  let term = inputField.value;
+
+  fetch(url + term)
+      .then((response) => response.json())
+      .then((data) => {
+          const fetchedResult = data.results;
+          return fetchedResult.map((result) => {
 
 
-inputField.addEventListener('submit', (event) => {
+            artistResult.innerHTML += `
+               
+                    <div class="audioContainter" id="content" data-id="song">
+<div class="artist">${result.artistName} </div>
+<div class= "track">${result.trackName}</div>
+<img id="player" src= ${result.artworkUrl100.replace("100x100", "280x280")}></div>
+<button class="playBack" id="playback" dataset-id =${result.trackId}>PLAY</button> 
+</div>
+`
+
+const audio = document.getElementById("playback");
+console.log(audio)
+audio.addEventListener('click', function (event) {
     event.preventDefault();
-    let term = inputField.value;
-  
-    document.addEventListener('play', event => {
-      const audio = document.getElementsByTagName('audio');
-       })
-    
-    while (container.firstChild) {
-          container.removeChild(container.firstChild);
-      }
-    
-  
-    fetch(itunesUrl + term + "&media=music" + "&limit=42" + "&country=us").then((response) => {
-      if (response.status === 200){
-        return response.json();
-      } else {
-        let errorMsg = document.createElement('h2');
-        errorMsg.innerText = 'Dust yourself off and try again.';
-        results.appendChild(errorMsg);
-      }
-    }).then((parsedJsonResponse) => {
-      console.log(parsedJsonResponse);
-      const songs = parsedJsonResponse.results;
-      return songs.map(results => {
-          const songCard = document.createElement("div"),    
-          artist = document.createElement('h2'),
-          song = document.createElement('h2'),
-          img = document.createElement('img'),
-          playButton = document.createElement('button')
-  
-          playButton.innerText = "Play Preview"
-  
-          playButton.addEventListener('click', (event) => {
-            event.preventDefault();
-            playPreview.src = results.previewUrl;
-            songTitle.innerText += " " + results.songName;
-          })
-  
-      artist.innerHTML = results.artistName;
-      track.innerHTML = results.songName;
-      img.src = results.artworkUrl100;
-      
-      musicCard.appendChild(img);
-      musicCard.appendChild(artist);
-      musicCard.appendChild(song);
-      musicCard.appendChild(playButton);
-      
-      container.appendChild(musicCard);
-  })
-  })
-    })
+    audioPlay.src = result.previewUrl;
+})
+});
+
+
+
+});
+
+
+
+});
